12911b6970e73d86d5ad20a48b576e90235d8e0f
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-09 00:52:40 +0300

    Pycodestyle checked

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
-# parent class BaseModel that defines common attributes/methods for other classes
+#!/usr/bin/python3
+# parent class BaseModel with attributes/methods for other classes
 
 import uuid
 from datetime import datetime
@@ -12,26 +13,25 @@ class BaseModel():
    created_at : the datetime at creation
    updated_at : the datetime of last update
     """
- 
+
+
 def __init__(self, *args, **kwargs):
     """Initializes class instances
-    
     If kwargs is not empty its creates an instance
-    
     """
-if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at':
-                    created = datetime.fromisoformat(value)
-                    self.created_at = created
-                elif key == 'updated_at':
-                    updated = datetime.fromisoformat(value)
-                    self.updated_at = updated
-                else:
-                    setattr(self, key, value)
-else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    if len(kwargs) != 0:
+        for key, value in kwargs.items():
+            if key == '__class__':
+                continue
+            elif key == 'created_at':
+                created = datetime.fromisoformat(value)
+                self.created_at = created
+            elif key == 'updated_at':
+                updated = datetime.fromisoformat(value)
+                self.updated_at = updated
+            else:
+                setattr(self, key, value)
+    else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
