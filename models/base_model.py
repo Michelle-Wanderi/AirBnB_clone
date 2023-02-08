bcaf9ccebf30fea1fc3953039c4e1d4067cc4298
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-09 02:03:07 +0300

    Added base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -14,24 +14,44 @@ class BaseModel():
    updated_at : the datetime of last update
     """
 
+    def __init__(self, *args, **kwargs):
+        """Initializes class instances
+        If kwargs is not empty its creates an instance
+        """
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    created = datetime.fromisoformat(value)
+                    self.created_at = created
+                elif key == 'updated_at':
+                    updated = datetime.fromisoformat(value)
+                    self.updated_at = updated
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-def __init__(self, *args, **kwargs):
-    """Initializes class instances
-    If kwargs is not empty its creates an instance
-    """
-    if len(kwargs) != 0:
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            elif key == 'created_at':
-                created = datetime.fromisoformat(value)
-                self.created_at = created
-            elif key == 'updated_at':
-                updated = datetime.fromisoformat(value)
-                self.updated_at = updated
-            else:
-                setattr(self, key, value)
-    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+    def __str__(self):
+        """String representation of the objects of this class"""
+        return ("[{}] {} {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
+
+    def save(self):
+        """updates the public instance attribute updated_at
+        with the current datetime
+        """
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values
+        of __dict__ of the instance:
+        """
+        properties = self.__dict__
+        properties["__class__"] = self.__class__.__name__
+        properties["created_at"] = self.created_at.isoformat()
+        properties["updated_at"] = self.updated_at.isoformat()
+        return properties
