f42738edb3406d433420ed1b75c852fbf038d3df
Michelle-Wanderi <michellewangari12@gmail.com>
2023-02-09 20:58:42 +0300

    changes in the console

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 #To see it in action cd into the airbnb console directory then run python commandline.py or python3 commandline.py in your terminal 
 
 import cmd
-import sys
+from models.base_model import BaseModel
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,23 +11,34 @@ class HBNBCommand(cmd.Cmd):
     intro = 'Welcome to the interpreter! Type help or ? to list commands.\n'
     prompt = '(hbnb) '
     
-    def do_hello(self, args):
+    def do_hello(self, line):
         """Says hello to the user"""
         print("Hello!")
 
 
-    def do_create(self, args):
+    def do_create(self, line):
         """Creates a new object"""
         print("Create a new object")
+        if len(line)==0:
+            print("**PLease write the name of the class")
+        elif line not in HBNBCommand.classes:
+            print("Class does not exist")
+        else:
+            instance = eval(line)()
+            instance.save()
+            print(instance.id)
 
-    def do_update(self, args):
+
+    def do_update(self, line):
         """Updates attributes of an object"""
         print("Update attributes of an object")
 
 
-    def do_destroy(self, args):
+    def do_destroy(self, line):
         """Destroys an object"""
         print("Destroy an object")
+        args = parse(line)
+
 
     def do_operations(self,args):
         """Do operations on objects"""
