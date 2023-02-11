29039062711cdeb76c4635da8a20315c25bf9625
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-11 09:02:47 +0300

    Fixed pycodestyling

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+
 """serializes instances to a JSON file and
     deserializes JSON file to instances
 """
