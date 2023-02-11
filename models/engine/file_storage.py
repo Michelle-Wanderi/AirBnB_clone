942cc71b3ec85b6f2b7b959e2a3c363b9f2e56be
Ebenezer <akhonyaebenezer@gmail.com>
2023-02-11 09:02:11 +0300

    File Storage Engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,7 +7,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-
+from models.review import Review
 """serializes instances to a JSON file and
     deserializes JSON file to instances
 """
@@ -27,7 +27,8 @@ class FileStorage:
         "State": State,
         "City": City,
         "Amenity": Amenity,
-        "Place": Place
+        "Place": Place,
+        "Review": Review
     }
 
     def all(self):
